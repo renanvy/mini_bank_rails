5cc076ad73e33e72ddeafa82c7926f50ac1a44fe
Renan <renanvy@gmail.com>
2015-06-09 19:43:34 -0300

    validates value in withdrawal

--- app/business/withdrawal.rb
+++ app/business/withdrawal.rb
@@ -3,7 +3,7 @@ class Withdrawal
 
   attr_accessor :value, :current_bank_account_id
 
-  validates :value, presence: true
+  validates :value, presence: true, numericality: { greater_than: 0.0 }
 
   def self.process(withdrawal_params)
     new(withdrawal_params).tap(&:process)
