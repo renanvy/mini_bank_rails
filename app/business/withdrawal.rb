0e2b7958dbb2107dab89749afc1a5e31fa18c84c
Renan <renanvy@gmail.com>
2015-06-09 20:01:00 -0300

    validate bank account balance

--- app/business/withdrawal.rb
+++ app/business/withdrawal.rb
@@ -4,6 +4,7 @@ class Withdrawal
   attr_accessor :value, :current_bank_account_id
 
   validates :value, presence: true, numericality: { greater_than: 0.0 }
+  validate  :bank_account_balance
 
   def self.process(withdrawal_params)
     new(withdrawal_params).tap(&:process)
@@ -11,8 +12,6 @@ class Withdrawal
 
   def process
     return unless valid?
-
-    current_bank_account = BankAccount.find(current_bank_account_id)
 
     current_bank_account.balance -= value.to_d
     current_bank_account.save!
@@ -22,12 +21,20 @@ class Withdrawal
 
   private
 
+  def current_bank_account
+    BankAccount.find(current_bank_account_id)
+  end
+
   def transaction
     # @transaction ||= Transaction.new(...........)
   end
 
   def save_transaction
     # transaction.save!
+  end
+
+  def bank_account_balance
+    errors.add(:value, "Saldo insuficiente") if value.to_d > current_bank_account.balance
   end
 
 end
