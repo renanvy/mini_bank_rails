e7fd2e14ad43c8ea6671259a604255ee9ba60265
Renan <renanvy@gmail.com>
2015-06-09 22:58:19 -0300

    some changes

--- app/models/bank_account.rb
+++ app/models/bank_account.rb
@@ -3,4 +3,7 @@ class BankAccount < ActiveRecord::Base
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
+
+  validates :name, :number, presence: true
+  validates :number, uniqueness: true, on: :create
 end
