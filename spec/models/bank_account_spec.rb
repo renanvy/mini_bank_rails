20e0789d5539782feb31f7531200e5fd6ecfe8ad
Renan <renanvy@gmail.com>
2015-06-11 22:36:46 -0300

    test method: generate_account_number

--- spec/models/bank_account_spec.rb
+++ spec/models/bank_account_spec.rb
@@ -1,5 +1,24 @@
 require 'rails_helper'
 
 describe BankAccount, type: :model do
+
   
+  
+  context "validations" do
+    it { should validate_presence_of(:name) }
+  end
+
+  context "associations" do
+    it { should have_many(:movimentations) }
+  end
+
+  describe "#generate_account_number" do
+  
+    let(:bank_account) { build(:bank_account, number: nil) }
+
+    it do
+      bank_account.save!
+      expect(bank_account.number).not_to be_nil 
+    end
+  end
 end
