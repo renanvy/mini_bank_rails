197f9a4eff8d2f8a79986496067131463b2a8d73
Renan Carneiro <renanvy@gmail.com>
2015-06-19 12:23:20 -0300

    rename project

--- config/application.rb
+++ config/application.rb
@@ -15,7 +15,7 @@ require "sprockets/railtie"
 # you've limited to :test, :development, or :production.
 Bundler.require(*Rails.groups)
 
-module TesteIntegraGrp
+module TMiniBankRails
   class Application < Rails::Application
     # Use the responders controller from the responders gem
     config.app_generators.scaffold_controller :responders_controller
--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -1,3 +1,3 @@
 # Be sure to restart your server when you modify this file.
 
-Rails.application.config.session_store :cookie_store, key: '_teste-integra-grp_session'
+Rails.application.config.session_store :cookie_store, key: '_mini_bank_rails_session'
